87e97920a36e4ad37a55881433a76f88a4d8f8c1
katinas15 <41838906+katinas15@users.noreply.github.com>
2020-11-02 01:05:36 +0200

    ...

--- Assets/EditorSnap.cs
+++ Assets/EditorSnap.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
+[SelectionBase]
 public class EditorSnap : MonoBehaviour
 {
     [SerializeField] [Range(1f, 20f)] float gridSize = 10f;
+    TextMesh textMesh;
 
+    void Start() {
+        
+    }
     void Update()
     {
         Vector3 snapPos;
-        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
-        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / (gridSize * gridSize)) * gridSize * gridSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / (gridSize * gridSize)) * gridSize * gridSize;
+
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = snapPos.x/ gridSize/ gridSize + ", " + snapPos.z / gridSize / gridSize;
 
         transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
     }
